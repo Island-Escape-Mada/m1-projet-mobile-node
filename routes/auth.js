fc362a1f25672b0d04bb48414afbcf56304a74ee
Nantenaina Rafidy~ <nantyrafidy@gmail.com>
2023-08-06 18:20:04 +0300

    user settings database persist

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 const router = express.Router();
@@ -8,21 +8,19 @@ router.post("/register", async (req, res) => {
 	try {
 		const { username, password } = req.body;
 
-		// Check if the username already exists
 		const existingUser = await User.findOne({ username });
 		if (existingUser) {
 			return res.status(400).json({ message: "Username already exists" });
 		}
 
-		// Hash the password before saving it to the database
+		// Hash the password before saving
 		// const hashedPassword = await bcrypt.hash(password, 10);
 
-		// Create a new user in the database
 		// const newUser = new User({ username, password: hashedPassword });
 		const newUser = new User({ username, password });
 		await newUser.save();
 
-		res.status(201).json({ message: "Registration successful", user: newUser.username });
+		res.status(201).json({ message: "Registration successful", user: newUser });
 	} catch (error) {
 		res.status(500).json({ message: "Registration failed", error: error.message });
 	}
@@ -32,19 +30,18 @@ router.post("/login", async (req, res) => {
 	try {
 		const { username, password } = req.body;
 
-		// Find the user in the database
 		const user = await User.findOne({ username });
 		if (!user) {
-			return res.status(401).json({ message: "Invalid username or password" });
+			return res.status(401).json({ message: "Invalid username" });
 		}
 
-		// Compare the password with the hashed password in the database
+		// Compare the provided password with the hashed password in the database
 		// const isPasswordValid = await bcrypt.compare(password, user.password);
 		if (password !== user.password) {
-			return res.status(401).json({ message: "Invalid username or password" });
+			return res.status(401).json({ message: "Invalid password" });
 		}
 
-		res.json({ message: "Login successful", user: user.username });
+		res.json({ message: "Login successful", user });
 	} catch (error) {
 		res.status(500).json({ message: "Login failed", error: error.message });
 	}
