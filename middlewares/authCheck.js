a8f6d94904909ee07ab2313d7b2843df5969d4b4
Famenontsoa <famenontsoa@outlook.com>
2023-08-07 15:16:05 +0300

    Authorization fixing

--- middlewares/authCheck.js
+++ middlewares/authCheck.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 const config = require('../configs/user.config');
 
 authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['Authorization']
+  const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
   if (token != config.token) {
     res.status(403).send("Access denied");
   }
-
+  next()
 }
 
 const checkToken = {
