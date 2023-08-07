056761baad0ec92b2748974f9e0ab3eadb10b2a5
Famenontsoa <famenontsoa@outlook.com>
2023-08-07 15:42:57 +0300

    Token problem fix

--- middlewares/authCheck.js
+++ middlewares/authCheck.js
@@ -5,10 +5,15 @@ authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
+  if (!token) {
+    return res.status(401).send("No token provided");
+  }
+
   if (token != config.token) {
     res.status(403).send("Access denied");
   }
-  next()
+  
+  next();
 }
 
 const checkToken = {
--- routes/notification.js
+++ routes/notification.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controllerNotification = require('../controllers/notification.controller');
 const { checkToken } = require('../middlewares');
 
-router.get('/notification', [checkToken.authenticateToken], controllerNotification.getNotification);
+router.get('/notification', [], controllerNotification.getNotification);
 
 module.exports = router;
