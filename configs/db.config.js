5366ed45bdebf39a27b8e0025991ab40dfa0a9d8
Famenontsoa <famenontsoa@outlook.com>
2023-08-08 14:14:38 +0300

    Upgrade to atlas

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     // CONNECTION: "mongodb://0.0.0.0:27017/island-escape-mada"
-    CONNECTION: "mongodb://ucl0hwvrn0cffo1d6zhf:xWMftT5nvKQB5LOh4Ik@bje0djx7tndsogg0yctt-mongodb.services.clever-cloud.com:2455/bje0djx7tndsogg0yctt"
+    CONNECTION: "mongodb+srv://famenontsoagreen:dse5BZJN4k5tli2w@cluster0.sbs4sxo.mongodb.net/"
   };
